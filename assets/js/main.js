56bdb4dd697fa19f1cc6ac78e2340d13dccb4fe1
CodeBrauer <gabrielwanzek@ymail.com>
2016-02-22 10:17:55 +0100

    Add javascript functions
    + getCookie by name
    + getURLID (/survery/2/ » returns "2")
    + highlightAnswer()
      -> checks cookie for answered survey and check it & disable all options

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,43 @@
 function navigateToSurvey(select) {
     window.location.href += '/../survey/' + select.value;
 }
+
+function highlightAnswer() {
+    var cookie = getCookie("survey_answered[" + getURLID() + "]");
+
+    if (cookie !== undefined) {
+        cookie     = decodeURIComponent(cookie);
+        cookieData = JSON.parse(cookie);
+        
+        var radios = document.querySelectorAll('input[type="radio"]');
+        [].forEach.call(radios, function(el, k) {   
+            el.setAttribute('disabled', 'disabled');
+            if (el.value == cookieData.answer_id) {
+                el.setAttribute('checked', 'checked');
+            }
+        })
+    }
+
+}
+
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function getURLID() {
+    var path    = document.location.pathname.split('/');
+    var results = new Array;
+
+    path.forEach(function(item, key) {
+        if (item !== "") {
+            results.push(item);
+        }
+    });
+
+    if (results[results.length - 1]) {
+        return results[results.length - 1];
+    }
+    return false;
+}
